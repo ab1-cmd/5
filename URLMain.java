882fd1a336a177a7ff5b27e3fb592c6e3f37d48c
ab1-cmd <cab031174@gmail.com>
2024-07-24 10:15:46 +0530

    Update URLMain.java

--- URLMain.java
+++ URLMain.java
@@ -1,18 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
-package anilkali;
 import java.net.URL;
-/**
- *
- * @author anilk
- */
+
 public class URLMain {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         try 
 {
